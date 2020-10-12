774cb83a8985078132cb4c6e24e462e72858d7f4
BeginnerYHang <1139050614@qq.com>
2020-10-12 19:33:10 +0800

    实力对比折线图

--- src/main/java/com/example/APP.java
+++ src/main/java/com/example/APP.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.example.mapper")
 public class APP {
-
     public static void main(String[] args) {
         SpringApplication.run(APP.class, args);
     }
