ec91dd17ba71b60483494ada6d97856a4795a4c6
BeginnerYHang <1139050614@qq.com>
2020-10-12 11:30:24 +0800

    筛选国家

--- src/main/java/com/example/APP.java
+++ src/main/java/com/example/APP.java
@@ -3,9 +3,7 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
-@EnableNeo4jRepositories
 @SpringBootApplication
 @MapperScan("com.example.mapper")
 public class APP {
